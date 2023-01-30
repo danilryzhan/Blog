70bb4a884116abfc5aac38f23c81266d78232180
bendymend <Ryzhandanil@gmail.com>
2023-01-29 23:05:29 -0500

    Add project files.

---
+++ Reactivities/Persistence/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text; 
+using System.Threading.Tasks;
+
+namespace Persistence
+{
+    internal class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions options):base(options) { 
+        
+        
+        }
+
+        public DbSet<Activity> activities { get; set; }
+    }
+}
